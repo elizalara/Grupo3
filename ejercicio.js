8e354f05fd4e00d5372028d155751e45966c6327
elizalara <eliza.lara95@gmail.com>
2016-10-07 01:32:44 -0500

    Ejercicios

---
+++ ejercicio.js
@@ -0,0 +1,25 @@
+function cambia1(){
+ 
+        document.getElementById('columna6').style.backgroundColor='orange';
+}
+
+function cambia3(){
+ 
+        document.getElementById('columna11').style.backgroundColor='orange';
+}
+function cambia4(){
+ 
+        document.getElementById('columna5').style.backgroundColor='orange';
+}
+function cambia5(){
+ 
+        document.getElementById('columna7').style.backgroundColor='orange';
+}
+function cambia6(){
+ 
+        document.getElementById('columna16').style.backgroundColor='orange';
+}
+function cambia7(){
+ 
+        document.getElementById('columna10').style.backgroundColor='orange';
+}
